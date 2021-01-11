88032ca23fff7de4f5a1e12ec83af21806f5feb4
verakash <akashverma150995@gmail.com>
2021-01-11 09:40:23 +0530

    added configuration to run the project on docker

--- api/user/models.py
+++ api/user/models.py
@@ -16,7 +16,7 @@ class CustomerUser(AbstractUser):
     USERNAME_FIELD= 'email'
     REQUIRED_FIELDS= []
 
-    phone = models.CharField(max_length=20 ,blank= True, null= True)
+    phone = models.CharField(max_length=10 ,blank= True, null= True)
     session_token= models.CharField(max_length=10 ,default=0)
 
     created_at= models.DateTimeField(auto_now_add=True)
